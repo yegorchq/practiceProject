317f4a49c30a6949a2c0f179b215c54a3845a867
yeska <KazantsevYS@mpei.ru>
2023-07-05 14:09:59 +0300

    первый вариант основной части

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 import numpy as np
-from relaxationMethod impirt relaxationMethod
-
-
+import time
+from sklearn.datasets import make_spd_matrix
+from relaxationMethod import relaxationMethod
+from soprGradMethod import soprGradMethod
 
 '''основная функция (подпрограмма), запускающая все тесты и получающая
  результаты замеров скорости выполнения [и точности] и визуализирующая
   эти результаты в графическом или табличном представлении.'''
 def main():
-  nList = [4, 8, 10, 20 , 40]
+  nList = [4, 8, 10, 20, 40]
   for n in nList:
-    generateMatrix(n)
+    matrix = generateMatrix(n)
+    b = np.random.randint(n)
+    timeValues = startAllFunc(matrix, b, n)
+    
+    
+    
+def generateMatrix(n):
+    matrix = make_spd_matrix(n)
+    return matrix
+    
+def startAllFunc(matrix, b, n)
+    '''
+    start_time = time.time()
+    relaxationMethod()
+    end_time = time.time()
+    time_relaxation = end_time - start_time
+    '''
+    '''
+    start_time = time.time()
+    minNevyazkaMethod()
+    end_time = time.time()
+    time_minNevyazka = end_time - start_time
+    '''
+    '''
+    start_time = time.time()
+    minNevyazkaMethod()
+    end_time = time.time()
+    time_minNevyazka = end_time - start_time
+    '''
+    start_time = time.time()
+    soprGradMethod()
+    end_time = time.time()
+    time_soprGrad = end_time - start_time
+    
+    timeValues = [time_relaxation, time_minNevyazka, time_soprGrad, time_Sonya]
+    return timeValues
---
+++ soprGradMethod.py
@@ -0,0 +1,7 @@
+#Егор
+def soprGradMethod(A: np.ndarray,
+  b: np.ndarray,
+  eps: np.float64) -> np.float64:
+
+  
+  return x
