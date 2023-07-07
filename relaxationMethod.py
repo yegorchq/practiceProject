2921fd7b68462ed6229d7b4aa016ad1cb3c8bc16
Fantan221 <stefan22810@inbox.ru>
2023-07-07 14:45:45 +0300

    Relax rabotiaet

--- relaxationMethod.py
+++ relaxationMethod.py
@@ -1,33 +1,23 @@
+#Стефан
 import numpy as np
-def relaxationMethod(A,b,eps):
-	
-	ITERATION_LIMIT = 1000
+def relaxationMethod(A, b, eps):
+  for i in range(A.shape[0]):
+      row = ["{0:3g}*x{1}".format(A[i, j], j + 1) for j in range(A.shape[1])]
 
-#system
-A = np.array([[1., 0., 0., 0.],
-          [0., 1., 0., 0.],
-          [0., 0., 1., 0.],
-          [0., 0., 0., 1.]
-          ])
-# vector b
-eps = 1e-8
-b = np.array([1., 1., 1., 1.])
+  x = np.zeros_like(b)
+  n = b.size
+  x0 = np.ones_like(b)
+  
+  for it_count in range(1, 20*n):
+      x_new = np.zeros_like(x)
+      for i in range(A.shape[0]):
+          s1 = np.dot(A[i, :i], x_new[:i])
+          s2 = np.dot(A[i, i + 1:], x[i + 1:])
+          x_new[i] = (b[i] - s1 - s2) / A[i, i]
+      if (np.sqrt(np.dot(x_new, x_new)/np.dot(x0, x0))) >= eps:
+          break
+      if (it_count == 2):
+        x0 = x_new
+      x = x_new
 
-print("System of equations:")
-for i in range(A.shape[0]):
-    row = ["{0:3g}*x{1}".format(A[i, j], j + 1) for j in range(A.shape[1])]
-    print("[{0}] = [{1:3g}]".format(" + ".join(row), b[i]))
-
-x = np.zeros_like(b)
-
-
-for it_count in range(1, ITERATION_LIMIT):
-    x_new = np.zeros_like(x)
-    print("Iteration {0}: {1}".format(it_count, x))
-    for i in range(A.shape[0]):
-        s1 = np.dot(A[i, :i], x_new[:i])
-        s2 = np.dot(A[i, i + 1:], x[i + 1:])
-        x_new[i] = (b[i] - s1 - s2) / A[i, i]
-    if np.allclose(x, x_new, rtol=eps):
-        break
-    x = x_new
+    return x
